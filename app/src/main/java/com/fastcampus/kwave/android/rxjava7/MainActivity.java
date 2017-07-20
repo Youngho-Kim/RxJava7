00f270717c616ca5962a17e46171a685da0cabb3
kwave <kwvae55@gmail.com>
2017-07-20 16:26:34 +0900

    오류 수정하기

--- app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
+++ app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 
 import com.fastcampus.kwave.android.rxjava7.domain.Data;
+import com.fastcampus.kwave.android.rxjava7.domain.Row;
 
 import io.reactivex.Observable;
 import io.reactivex.schedulers.Schedulers;
@@ -28,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // 1. 레트로핏 생성
-        Retrofit client = new Retrofit
-                .Builder().baseUrl(SERVER)
+        Retrofit client = new Retrofit.Builder()
+                .baseUrl(SERVER)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
@@ -38,21 +39,28 @@ public class MainActivity extends AppCompatActivity {
         IWeather service = client.create(IWeather.class);
 
         // 3. 옵저버블 생성
-        Observable<Data> observable = service.getData(SERVER_KEY, 1,10, "seocho");
+        Observable<Data> observable = service.getData(SERVER_KEY, 1,10, "서초");
 
         // 4. 발행 시작
         observable.subscribeOn(Schedulers.io())
                 .observeOn(Schedulers.newThread())
                 // 구독 시작
                 .subscribe(
-                        data -> Log.i("Weather","개수"+data.getRealtimeWeatherStation().getList_total_count())
+                        data -> {
+                            Row rows[] = data.getRealtimeWeatherStation().getRow();
+                            for(Row row : rows){
+                                Log.i("Weather","지역명"+row.getSTN_NM());
+                                Log.i("Weather","온도"+row.getSAWS_TA_AVG()+"도");
+                                Log.i("Weather","습도"+row.getSAWS_HD()+"%");
+                            }
+                        }
                 );
     }
 }
 
 interface IWeather{
-    @GET("{key}/xml/RealtimeWeatherstation/{start}/{count}/{name}")
-    Observable<Data> getData(@Path("key") String kerver_key
+    @GET("{key}/json/RealtimeWeatherStation/{start}/{count}/{name}")
+    Observable<Data> getData(@Path("key") String server_key
                            , @Path("start") int begin_index
                            , @Path("count") int offset
                            , @Path("name") String gu);
