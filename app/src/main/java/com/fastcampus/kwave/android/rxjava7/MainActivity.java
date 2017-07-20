992558ea3d6837497d3450d31a932e04e2695539
kwave <kwvae55@gmail.com>
2017-07-21 00:06:39 +0900

    ReadMe 작성하기

--- app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
+++ app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
@@ -63,21 +63,24 @@ public class MainActivity extends AppCompatActivity {
 
     public void btnClick() {
         btnFind.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
+
                 // 1. 레트로핏 생성
                 Retrofit client = new Retrofit.Builder()
-                        .baseUrl(SERVER)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                        .build();
+                        .baseUrl(SERVER)        // retrofit을 연결할 주소
+                        .addConverterFactory(GsonConverterFactory.create())         // Json Parser 추가
+                        // Observable을 리턴 타입으로 사용하기 위해서 RxJava2CallAdapterFactory를 어댑터로 사용
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())  //RxJava2를 사용하기 위해 추가
+                        .build();               //인터페이스 연결
 
                 // 2. 서비스 생성
                 IWeather service = client.create(IWeather.class);
 
                 // 3. 옵저버블 생성
                 Observable<Data> observable = service.getData(SERVER_KEY, 1,10, editFind.getText().toString());
-
+                                                                                                 // editFind.getText().toString() : 입력 받을 텍스트
                 // 4. 발행 시작
                 observable.subscribeOn(Schedulers.io())
                         .observeOn(AndroidSchedulers.mainThread())
@@ -85,7 +88,7 @@ public class MainActivity extends AppCompatActivity {
                         .subscribe(
                                 data -> {
                                     Row rows[] = data.getRealtimeWeatherStation().getRow();
-                                    weather.clear();
+//                                    weather.clear();      // 이전에 입력한 리스트를 없애고 현재 입력한 리스트만 나오게 함
                                     for(Row row : rows){
                                         Log.i("rows","=================rows개수 : "+rows.length);
                                         Weather weatherInfo = new Weather();
@@ -98,13 +101,12 @@ public class MainActivity extends AppCompatActivity {
 //                                Log.i("Weather","온도"+row.getSAWS_TA_AVG()+"도");
 //                                Log.i("Weather","습도"+row.getSAWS_HD()+"%");
                                     }
+                                    editFind.setText("");
                                     adapter.notifyDataSetChanged();
                                 }
                         );
-
             }
         });
-
     }
 
     public class WeatherAdapter extends RecyclerView.Adapter<Holder>{
@@ -146,11 +148,14 @@ public class MainActivity extends AppCompatActivity {
             this.data = weather;
         }
     }
+
+
     public class Holder extends RecyclerView.ViewHolder{
         int position;
         private TextView textName;
         private TextView textTemperture;
         private TextView textHumidity;
+
         public Holder(View itemView) {
             super(itemView);
             textName = (TextView) itemView.findViewById(R.id.textName);
@@ -163,7 +168,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void setTextName(String Name) {
-            textName.setText(Name);
+            textName.setText(Name+"구");
         }
 
         public String  getTextTemperture() {
@@ -171,7 +176,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void setTextTemperture(String Temperture) {
-            textTemperture.setText(Temperture+"도");
+            textTemperture.setText("온도 : " +Temperture+"도");
         }
 
         public String getTextHumidity() {
@@ -179,17 +184,16 @@ public class MainActivity extends AppCompatActivity {
         }
 
         public void setTextHumidity(String Humidity) {
-            textHumidity.setText(Humidity+"%");
+            textHumidity.setText("습도 : " +Humidity+"%");
         }
 
         public void setPosition(int position) {
             this.position = position;
         }
     }
-
 }
 
-interface IWeather{
+interface IWeather{     // Retrofit interface 선언
     @GET("{key}/json/RealtimeWeatherStation/{start}/{count}/{name}")
     Observable<Data> getData(@Path("key") String server_key
                            , @Path("start") int begin_index
