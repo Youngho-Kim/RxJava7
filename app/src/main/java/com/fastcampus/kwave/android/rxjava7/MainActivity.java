660dd22b4e049f7e24df604fbb1ef78a415dd9fa
kwave <kwvae55@gmail.com>
2017-07-20 22:11:32 +0900

    Retrofit을 이용해 실시간 날씨 데이터 갖고 오기

--- app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
+++ app/src/main/java/com/fastcampus/kwave/android/rxjava7/MainActivity.java
@@ -1,13 +1,27 @@
 package com.fastcampus.kwave.android.rxjava7;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.fastcampus.kwave.android.rxjava7.domain.Data;
 import com.fastcampus.kwave.android.rxjava7.domain.Row;
+import com.fastcampus.kwave.android.rxjava7.domain.Weather;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.schedulers.Schedulers;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
@@ -22,40 +36,157 @@ public class MainActivity extends AppCompatActivity {
 
     public static final String SERVER = "http://openAPI.seoul.go.kr:8088/";
     public static final String SERVER_KEY = "4a704a59546b776138307975426366";
+    public List<Weather> weather = new ArrayList<>();
+    WeatherAdapter adapter = null;
+    Button btnFind;
+    EditText editFind;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        // 1. 레트로핏 생성
-        Retrofit client = new Retrofit.Builder()
-                .baseUrl(SERVER)
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build();
-
-        // 2. 서비스 생성
-        IWeather service = client.create(IWeather.class);
-
-        // 3. 옵저버블 생성
-        Observable<Data> observable = service.getData(SERVER_KEY, 1,10, "서초");
-
-        // 4. 발행 시작
-        observable.subscribeOn(Schedulers.io())
-                .observeOn(Schedulers.newThread())
-                // 구독 시작
-                .subscribe(
-                        data -> {
-                            Row rows[] = data.getRealtimeWeatherStation().getRow();
-                            for(Row row : rows){
-                                Log.i("Weather","지역명"+row.getSTN_NM());
-                                Log.i("Weather","온도"+row.getSAWS_TA_AVG()+"도");
-                                Log.i("Weather","습도"+row.getSAWS_HD()+"%");
-                            }
-                        }
-                );
+        initView();
+        btnClick();
     }
+
+    private void initView(){
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        btnFind = (Button) findViewById(R.id.btnFind);
+        editFind = (EditText) findViewById(R.id.editFind);
+
+
+        adapter = new WeatherAdapter(weather,getBaseContext());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    public void btnClick() {
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 1. 레트로핏 생성
+                Retrofit client = new Retrofit.Builder()
+                        .baseUrl(SERVER)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                        .build();
+
+                // 2. 서비스 생성
+                IWeather service = client.create(IWeather.class);
+
+                // 3. 옵저버블 생성
+                Observable<Data> observable = service.getData(SERVER_KEY, 1,10, editFind.getText().toString());
+
+                // 4. 발행 시작
+                observable.subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        // 구독 시작
+                        .subscribe(
+                                data -> {
+                                    Row rows[] = data.getRealtimeWeatherStation().getRow();
+                                    weather.clear();
+                                    for(Row row : rows){
+                                        Log.i("rows","=================rows개수 : "+rows.length);
+                                        Weather weatherInfo = new Weather();
+                                        weatherInfo.STN_NM = row.getSTN_NM();
+                                        weatherInfo.SAWS_TA_AVG = row.getSAWS_TA_AVG();
+                                        weatherInfo.SAWS_HD = row.getSAWS_HD();
+                                        weather.add(weatherInfo);
+
+//                                Log.i("Weather","지역명"+row.getSTN_NM());
+//                                Log.i("Weather","온도"+row.getSAWS_TA_AVG()+"도");
+//                                Log.i("Weather","습도"+row.getSAWS_HD()+"%");
+                                    }
+                                    adapter.notifyDataSetChanged();
+                                }
+                        );
+
+            }
+        });
+
+    }
+
+    public class WeatherAdapter extends RecyclerView.Adapter<Holder>{
+        public List<Weather> data = null;
+        private LayoutInflater inflater = null;
+
+        public WeatherAdapter(List<Weather> data, Context context) {
+            this.data = data;
+            inflater =  LayoutInflater.from(context);
+        }
+
+        @Override
+        public Holder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View view = inflater.inflate(R.layout.item_list_weather, parent, false);
+            return new Holder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(Holder holder, int position) {
+            Weather weather = data.get(position);
+            Log.d("weather","----------------------------------------weather : "+weather);
+            holder.setTextName(weather.STN_NM);
+            holder.setTextTemperture(weather.SAWS_TA_AVG);
+            holder.setTextHumidity(weather.SAWS_HD);
+            holder.setPosition(position);
+            Log.d("holder","----------------------------------------holder : "+holder);
+        }
+
+        @Override
+        public int getItemCount() {
+            return data.size();
+        }
+
+        public Weather getItem(int position){
+            return data.get(position);
+        }
+
+        public void setData(List<Weather> weather) {
+            this.data = weather;
+        }
+    }
+    public class Holder extends RecyclerView.ViewHolder{
+        int position;
+        private TextView textName;
+        private TextView textTemperture;
+        private TextView textHumidity;
+        public Holder(View itemView) {
+            super(itemView);
+            textName = (TextView) itemView.findViewById(R.id.textName);
+            textTemperture = (TextView) itemView.findViewById(R.id.textTemperture);
+            textHumidity = (TextView) itemView.findViewById(R.id.textHumidity);
+        }
+
+        public String getTextName() {
+            return textName.getText().toString();
+        }
+
+        public void setTextName(String Name) {
+            textName.setText(Name);
+        }
+
+        public String  getTextTemperture() {
+            return textTemperture.getText().toString();
+        }
+
+        public void setTextTemperture(String Temperture) {
+            textTemperture.setText(Temperture+"도");
+        }
+
+        public String getTextHumidity() {
+            return textHumidity.getText().toString();
+        }
+
+        public void setTextHumidity(String Humidity) {
+            textHumidity.setText(Humidity+"%");
+        }
+
+        public void setPosition(int position) {
+            this.position = position;
+        }
+    }
+
 }
 
 interface IWeather{
---
+++ app/src/main/java/com/fastcampus/kwave/android/rxjava7/domain/Weather.java
@@ -0,0 +1,29 @@
+package com.fastcampus.kwave.android.rxjava7.domain;
+
+/**
+ * Created by kwave on 2017-07-20.
+ */
+
+public class Weather {
+    public String SAWS_SOLAR;
+
+    public String SAWS_OBS_TM;
+
+    public String SAWS_SHINE;
+
+    public String SAWS_WS_AVG;
+
+    public String NAME;
+
+    public String SAWS_HD;
+
+    public String STN_NM;
+
+    public String SAWS_RN_SUM;
+
+    public String STN_ID;
+
+    public String CODE;
+
+    public String SAWS_TA_AVG;
+}
